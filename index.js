bec5f033250ae66b23a3bc0b0a7d65380ca01cb3
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-11-28 11:53:16 +0100

    set up first routes

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
 const cors = require("cors");
-const data = require("./data.json");
+const PokemonList = require("./data.json");
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.get("/pokemon", (req, res) => {
+  res.json(data);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  const { id } = req.params;
+
+  const singlePokemon = PokemonList.find((pokemon) => {
+    if (pokemon.id === +id) {
+      return pokemon;
+    }
+  });
+  if (singlePokemon) {
+    res.json(singlePokemon);
+  } else {
+    res.status(404).json({ message: "Pokemon not found" });
+  }
+});
+
+app.get("/pokemon/:id/:info", (req, res) => {
+  const { id, type } = req.params;
+
+  const onePokemon = pokemon[type][id];
+
+
 });
 
 app.listen(port, () => {
