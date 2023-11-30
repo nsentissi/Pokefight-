af93e93667dbc3e2f3567323d0115ba015721b9c
Bernard Almeida da Costa <biernard@gmail.com>
2023-11-30 16:26:11 +0100

    updates on CSS

--- client/src/components/PokemonDetails.jsx
+++ client/src/components/PokemonDetails.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const PokemonDetails = () => {
-  return (
-    <div>
-      Detail page
-    </div>
-  )
-}
+  const [pokemon, setPokemon] = useState({});
 
-export default PokemonDetails
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/pokemon/${6}`)
+      .then((response) => {
+        console.log(response.data);
+        setPokemon(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="pokemon-img"></div>
+      {pokemon ? (
+        <div className="status-card">
+          <div>
+            {pokemon.name && <h1>{pokemon.name.english}</h1>}
+            {pokemon.id && (
+              <div>
+                <strong>ID:</strong> {pokemon.id}
+              </div>
+            )}
+
+            {pokemon.type && (
+              <div>
+                <strong>Type:</strong> {pokemon.type.join(", ")}
+              </div>
+            )}
+          </div>
+          <div>
+            <h3>Stats:</h3>
+            {pokemon.base && (
+              <div>
+                {Object.entries(pokemon.base).map(([key, value]) => (
+                  <div key={key}>
+                    <strong>{key}:</strong> {value}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      ) : (
+        // Render something if data is not available
+        <div>No Pokemon available</div>
+      )}
+    </div>
+  );
+};
+
+export default PokemonDetails;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 const pokemonRouter = require("./routes/pokemon.js");
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/pokemon", pokemonRouter);
-
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
