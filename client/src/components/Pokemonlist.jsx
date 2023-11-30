50061373a867093db1e35950abf526855df0bb34
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-11-30 14:26:53 +0100

    added the pokemon logo type and displayed them in cards

--- client/src/components/Pokemonlist.jsx
+++ client/src/components/Pokemonlist.jsx
@@ -1,11 +1,87 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Colorless from "../assets/Colorless.png";
+import Dark from "../assets/Dark.png";
+import Dragon from "../assets/Dragon.png";
+import Electric from "../assets/Electric.png";
+import Fairy from "../assets/Fairy.png";
+import Fighting from "../assets/Fighting.png";
+import Fire from "../assets/Fire.png";
+import Grass from "../assets/Grass.png";
+import Metal from "../assets/Metal.png";
+import Psychic from "../assets/Psychic.png";
+import Water from "../assets/Water.png";
+import Bug from "../assets/Bug.png";
+import Normal from "../assets/Bug.png";
+import Poison from "../assets/Poison.png";
+import Ground from "../assets/Ground.png";
+import Rock from "../assets/Rock.png";
+import Ghost from "../assets/Ghost.png";
+import Ice from "../assets/Ice.png";
 
 const Pokemonlist = () => {
+  const [pokemons, setPokemons] = useState(null);
+  const [pokemonData, setPokemonData] = useState(null);
+
+  const typeImages = {
+    Colorless,
+    Dark,
+    Dragon,
+    Electric,
+    Fairy,
+    Fighting,
+    Fire,
+    Grass,
+    Metal,
+    Psychic,
+    Water,
+    Bug,
+    Normal,
+    Poison,
+    Ground,
+    Rock,
+    Ghost,
+    Ice,
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/pokemon")
+      .then((response) => {
+        setPokemons(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
-      Pokemon list 
+      {!pokemons ? (
+        <p>Loading ... </p>
+      ) : (
+        <div className="PokeCard">
+          {pokemons.map((pokemon) => (
+            <div key={pokemon.id} className="pokemon-card">
+              <img
+                className="pokemon-icon"
+                src={typeImages[pokemon.type[0]]}
+               
+                alt="Pokemon Icon"
+              />
+              <img
+                className="pokemon-image"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`}
+                alt={`Image of ${pokemon.name.english}`}
+              />
+              <h2 className="pokemon-name">{pokemon.name.english}</h2>
+              <p className="pokemon-type">{pokemon.type[0]}</p>
+              <button className="view-more-button">View More</button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Pokemonlist
+export default Pokemonlist;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 const pokemonRouter = require("./routes/pokemon.js");
 
 app.use(express.json());
+app.use(cors());
 app.use("/pokemon", pokemonRouter);
 
 app.listen(port, () => {
