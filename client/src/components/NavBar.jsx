d48f714fcbd03cff4f894467d7434311d6d7329d
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-12-05 21:44:04 +0100

    added video and fixed the navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 import "./navbar.css";
-import "./Homepage";
 import { Link } from "react-router-dom";
 
+
 export default function NavBar() {
-  return (
+    return (
     <nav className="navbar">
-      <Link to="/">Homepage</Link>
-      <Link to="/pokemon">Pokemonlist</Link>
-      <Link to="/contact">Contact</Link>
-      <span></span>
+        <Link to="/">Homepage</Link>
+        <Link to="/pokemon">Pokemonlist</Link>
+        <Link to="/contact">Contact</Link>
+        <span></span>
     </nav>
-  );
+);
 }
