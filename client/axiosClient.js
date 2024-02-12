8be1b32e334ad5bfc46cf7752c8391affc411035
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-02-12 12:15:48 -0500

    fixed axios client in front end

--- client/axiosClient.js
+++ client/axiosClient.js
@@ -3,7 +3,6 @@ import axios from "axios"
 
 const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_BACKENDURL || ""}/api`,
-    withCredentials:true
 })
 
 export default axiosClient
